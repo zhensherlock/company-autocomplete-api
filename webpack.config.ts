0d0ab057f5376322e128338342a69ae23f1df925
MichaelSun <zhensherlock@126.com>
2023-05-09 10:42:36 +0800

    feat: add webpack

---
+++ webpack.config.ts
@@ -0,0 +1,25 @@
+import path from 'path'
+import nodeExternals from 'webpack-node-externals'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+
+module.exports = {
+  entry: './src/app.ts', // 入口文件
+  target: 'node', // 告诉Webpack打包为Node.js运行环境
+  mode: 'production', // 设置打包模式
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // 输出文件名
+  },
+  externals: [nodeExternals()], // 排除Node.js核心模块和其他特定于Node.js的模块
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
