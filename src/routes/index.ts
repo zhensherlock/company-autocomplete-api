20fef1e918326508c051a8e0dac8334f56393528
MichaelSun <zhensherlock@126.com>
2024-01-08 10:47:01 +0800

    feat: update qcc-kyc api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import Router from '@koa/router';
 import qccRouter from './qcc'
 // import tycRouter from './tianyancha'
 import qccOpenRouter from './qcc-open'
-import qccKYCRouter from './qcc-global'
+import qccGlobalRouter from './qcc-global'
 // import qxbRouter from './qxb'
 
 const router = new Router()
@@ -11,7 +11,7 @@ const router = new Router()
 router.use('/qcc', qccRouter.routes())
 // router.use('/tyc', tycRouter.routes())
 router.use('/qcc-open', qccOpenRouter.routes())
-router.use('/qcc-global', qccKYCRouter.routes())
+router.use('/qcc-global', qccGlobalRouter.routes())
 // router.use('/qxb', qxbRouter.routes())
 
 export default router
